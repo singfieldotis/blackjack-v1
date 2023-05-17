a2b51295e667051e9bc36ff3f89f21d8b26b9c89
Otis Singfield <singfieldotis@gmail.com>
2023-05-16 20:44:21 -0400

    More game logic
    added new card button and added new card to player sum

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
-let firstCard = 10
+let firstCard = 2
 let secondCard = 10
+
+let cards = [firstCard, secondCard]
+
 let sum = firstCard + secondCard 
 
 let hasBlackJack = false
 let isAlive = true
 
 let message = ""
+let messageEl = document.getElementById("message-el")
 
-if (sum <= 20) {
-    message ="Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "Bust! You're out of the game! 😭"
-    isAlive = false
+ let sumEl = document.getElementById("sum-el")
+// let sumEl = document.querySelector("#sum-el")
+
+let cardEl = document.getElementById("card-el")
+
+function startGame() {
+    renderGame()
 }
 
-console.log(message)
+function renderGame() {
+    cardEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    sumEl.textContent = "Sum: " + sum
+    
+    if (sum <= 20) {
+        message ="Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "Bust! You're out of the game!"
+        isAlive = false
+    }
+    
+    messageEl.textContent = message
+}
+
+
+
+function newCard() {
+    console.log("Drawing a new card from the deck...")
+    let card = 4
+    sum = sum + card 
+    renderGame()
+} 
